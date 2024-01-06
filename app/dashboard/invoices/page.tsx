e433b92a9ab971639f2560f9642bb17e2846d489
elvin <ali.elvin@gmail.com>
2024-01-06 22:13:32 +0000

    Ch.9 Streaming

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-    return <p>Invoices Page</p>;
-  }
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Invoices Page
+      </h1>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <LatestInvoices />
+      </div>
+    </main>
+  );
+}
